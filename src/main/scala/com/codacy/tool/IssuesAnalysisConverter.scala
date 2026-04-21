0981a388a566b9854656a606e6b66897729171b0
Hélio Rocha <helio@codacy.com>
2026-04-21 17:13:24 +0100

    Replace default codec with UTF8

--- src/main/scala/com/codacy/tool/IssuesAnalysisConverter.scala
+++ src/main/scala/com/codacy/tool/IssuesAnalysisConverter.scala
@@ -4,12 +4,13 @@ import java.nio.file.Path
 
 import com.codacy.analysis.core.model.{IssuesAnalysis, ToolResults}
 import com.codacy.analysis.core.serializer.IssuesReportSerializer
+import scala.io.Codec
 
 trait IssuesAnalysisConverter {
   protected def convert(lines: Seq[String], relativizeTo: Path): IssuesAnalysis
 
   def run(toolName: String, config: ParserConfig): Unit = {
-    val stdin = scala.io.Source.fromInputStream(System.in)(config.encoding)
+    val stdin = scala.io.Source.fromInputStream(System.in)(Codec(config.encoding))
     val lines = stdin.getLines().to(LazyList)
     val issuesResults = convert(lines, relativizeTo = config.relativizeTo)
     val toolResults = ToolResults(toolName, issuesResults)
